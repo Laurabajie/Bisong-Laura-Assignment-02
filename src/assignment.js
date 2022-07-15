1d1f5954e0fb0ca26f42c45506782e1c822d4e1d
Laurabajie <105749573+Laurabajie@users.noreply.github.com>
2022-07-15 14:50:20 +0000

    Assignment Challenge 1 and 2

--- src/assignment.js
+++ src/assignment.js
@@ -14,9 +14,18 @@ const assignment = {};
  * @returns number the sum of the numbers
  */
 function sumOfNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+  let total = 0;
+
+  for (
+    let i = 0;
+    i < arrayOfNumbers.length;
+    i += 1
+  ) {
+    total += arrayOfNumbers[i]
+  }
+  return total;
 }
-// assignment.sumOfNumbers = sumOfNumbers;
+assignment.sumOfNumbers = sumOfNumbers;
 
 /**
  * Challenge - 2
@@ -28,9 +37,19 @@ function sumOfNumbers(arrayOfNumbers) {
  * @returns number the count of even numbers
  */
 function countEvenNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+  let evenNumbers = 0;
+  for (let MoMo = 0;
+      MoMo < arrayOfNumbers.length;
+      MoMo ++)
+    {
+      if (arrayOfNumbers[MoMo] % 2 == 0)
+      {
+        evenNumbers ++
+      }
+    }
+    return evenNumbers;
 }
-// assignment.countEvenNumbers = countEvenNumbers;
+assignment.countEvenNumbers = countEvenNumbers;
 
 /**
  * Challenge - 3
